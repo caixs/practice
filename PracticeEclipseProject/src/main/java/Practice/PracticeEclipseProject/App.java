aaaba379da2c5235c280d733b436c13b0cb05c39
xAdmin <xAdmin@KOSMOSII>
2021-04-09 12:56:07 -0700

    test12345

--- PracticeEclipseProject/src/main/java/Practice/PracticeEclipseProject/App.java
+++ PracticeEclipseProject/src/main/java/Practice/PracticeEclipseProject/App.java
@@ -7,7 +7,7 @@ package Practice.PracticeEclipseProject;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {	//test1235
         System.out.println( "Hello World!" );
     }
 }
